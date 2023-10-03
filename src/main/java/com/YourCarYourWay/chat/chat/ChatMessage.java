451cf7e3290c602849a377994af3ab83ed2ae8ec
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-10-03 16:14:52 +0200

    Implement the char controller

---
+++ src/main/java/com/YourCarYourWay/chat/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.YourCarYourWay.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType messageType;
+}
---
+++ src/main/java/com/YourCarYourWay/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.YourCarYourWay.chat.chat;
+
+public enum MessageType {
+    CHAT,   // When the user use the chat
+    JOIN,   // When the user join the chat
+    LEAVE   // When the user leave the chat
+}
